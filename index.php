07c8ab440494e5d7fdb6c5a575112c5868e77e1e
Oleg <oarch@mail.ru>
2015-02-26 20:33:28 +0300

    Контроллер входа

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
-echo 'Привет !';
+require_once __DIR__ . '/settings/autoload.php';
+
+$ctrl = (!empty($_GET['ctrl'])) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+
+$controllerClassName = $ctrl . 'Controller';
+$controller = new $controllerClassName;
+$method = 'action'.$act;
+
+$controller->$method();
+;
