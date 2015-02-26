2d2065248d6d798b4bdca1e089ded4592188bcb4
Oleg <oarch@mail.ru>
2015-02-26 20:30:25 +0300

    Класс базы данных

---
+++ models/db.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__. '/../functions/sql.php';
+
+class DB{
+
+    public function __construct(){
+        mysql_connect('localhost','root','');
+        mysql_select_db('library');
+    }
+    public function queryAll($sql, $class='stdClass'){
+
+        $res = mysql_query($sql);
+        if (false === $res){
+            return false;
+        }
+        $arr = [];
+        while ($obj = mysql_fetch_object($res, $class)){
+            $arr[] =  $obj;
+        }
+        return $arr;
+    }
+
+    public function queryOne($sql, $class='stdClass'){
+        return $this->queryAll($sql, $class)[0];
+    }
+
+    public function queryAdd($news){
+
+        $query = "INSERT INTO news ( title, text )
+                        VALUES('$news->title', '$news->text')";
+        if ($res = mysql_query($query)){
+            return true;
+        }
+        echo 'query false';
+        return false;
+    }
+}
